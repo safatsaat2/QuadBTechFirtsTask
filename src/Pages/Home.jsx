38f9fb732c8dbe9916145de0fd0f8142ce6c17b4
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-08-07 22:05:56 +0600

    added:home container card html

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-
+import { useState } from 'react';
 const Home = () => {
+    const [data, setData] = useState([])
+
+    fetch('https://api.tvmaze.com/search/shows?q=all')
+        .then(res => res.json())
+        .then(res => setData(res))
+
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <>
+            <div className="homeContainer">
+                <div className="cardShow">
+                    <img src="https://static.tvmaze.com/uploads/images/medium_portrait/413/1034988.jpg" alt="" />
+                    <div className='cardInfo'>
+                        <div>
+                            <h3>Name</h3>
+                            <p>genres</p>
+                        </div>
+                        <button>See More</button>
+                        <div>
+                            <Icon icon="bx:star" color="#ffe234" /> <span>star</span>
+                            <p>premiered</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
